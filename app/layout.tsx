4a6e5aaed755116faeca8d3ad9d343030d2bee00
Thales <thales.penha@edu.ufes.br>
2026-05-15 16:54:54 -0300

    TRAFEGO: add google tags pro trafego pago

--- app/layout.tsx
+++ app/layout.tsx
@@ -398,6 +398,17 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${lato.variable} ${nunito.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNGVFWC8');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="image_src" href="https://psicointelekta.com.br/og-image.png" />
         <script
           type="application/ld+json"
@@ -407,6 +418,16 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNGVFWC8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
         <SpeedInsights />
